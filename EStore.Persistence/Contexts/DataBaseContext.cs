be78305a1ce3820f0ef93c91c4509be71b6a9b50
shahram afshar <shahram.afshar1369@hotmail.com>
2022-05-05 19:42:09 +0430

    insert dbser user and role and user in role

---
+++ EStore.Persistence/Contexts/DataBaseContext.cs
@@ -0,0 +1,23 @@
+﻿using EStore.Domain.Entities.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore.Persistence.Contexts
+{
+   public class DataBaseContext:DbContext
+    {
+        public DataBaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<UserInRole> UserInRoles { get; set; }
+
+    }
+}
