fb8d8eab3c5da9783475726a87faa03faf20c3f2
shahram afshar <shahram.afshar1369@hotmail.com>
2022-05-13 19:39:11 +0430

    seperat class for service remove user

---
+++ EStore.Application/Services/Users/Commands/RemoveUser/RemoveUserService.cs
@@ -0,0 +1,38 @@
+﻿using EStore.Application.Interfaces.Contexts;
+using EStore.Common.Dto;
+using System;
+
+namespace EStore.Application.Services.Users.Commands.RemoveUser
+{
+    public class RemoveUserService : IRemoveUserService
+    {
+        private readonly IDataBaseContext _contex;
+
+        public RemoveUserService(IDataBaseContext context)
+        {
+            _contex = context;
+        }
+        public ResultDto Execute(int userId)
+        {
+            var user = _contex.Users.Find(userId);
+
+            if (user == null)
+            {
+                return new ResultDto() { 
+                IsSuccess=false,
+                Message="کاربری با این مشخصات یافت نشد"
+                };
+            }
+            else
+            {
+                user.RemoveTime = DateTime.Now;
+                user.IsRemove = true;
+                _contex.SaveChanges();
+                return new ResultDto() { 
+                IsSuccess=true,
+                Message="عملیات حذف کاربر با موفقیت انجام شد"
+                };
+            }
+        }
+    }
+}
