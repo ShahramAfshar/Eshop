f495e8aed57daf08d7ea2431d2aa9fd809f38b01
shahram afshar <shahram.afshar1369@hotmail.com>
2022-05-13 19:27:29 +0430

    Add Class Base Entity

---
+++ EStore.Domain/Entities/Commons/BaseEntityNotId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore.Domain.Entities.Commons
+{
+    public class BaseEntityNotId
+    {
+        public DateTime InsertTime { get; set; } = DateTime.Now;
+        public DateTime? UpdateTime { get; set; }
+        public bool IsRemove { get; set; } = false;
+        public DateTime RemoveTime { get; set; }
+    }
+}
--- EStore.Domain/Entities/Users/User.cs
+++ EStore.Domain/Entities/Users/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EStore.Domain.Entities.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,8 @@ using System.Threading.Tasks;
 
 namespace EStore.Domain.Entities.Users
 {
-    public class User
+    public class User:BaseEntity
     {
-        public int Id { get; set; }
         public string  FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
